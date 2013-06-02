a30b303ca606d6fe11b3c7551de0205a11f3d516
Clif Bratcher <cebratcher@gmail.com>
2013-06-02 18:12:55 -0400

    Random library

---
+++ js/random.js
@@ -0,0 +1,32 @@
+/* Random
+ *
+ * Utility object for random number generation.
+ *
+ * 2013 - Clif Bratcher
+ *
+ */
+var Random = new function() {
+    this.int = function(max) {
+        return Math.floor(this.float(max));
+    };
+
+    this.float = function(max) {
+        return Math.random() * max;
+    };
+
+    this.int = function(max) {
+        return Math.floor(this.float(max));
+    };
+
+    this.bool = function() {
+        return this.int(2);
+    };
+
+    this.intRange = function(min, max) {
+        return min + Random.int(max - min + 1);
+    };
+
+    this.floatRange = function(min, max) {
+        return min + Random.float(max - min + 1);
+    };
+};
